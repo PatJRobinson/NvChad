d7d23098b6319754bf9da241fb7ecdef68e11ead
PatJRobinson <robinsonpaddy4@gmail.com>
2025-02-22 23:30:27 +0000

    adds DAP debuging config for C++

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -2,5 +2,5 @@
 local M = {}
 M.plugins = "custom.plugins"
 M.ui = { theme = 'one_light' }
-
+M.mappings = require("custom.mappings")
 return M
--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,5 +1,44 @@
 local plugins = {
   {
+    "nvim-neotest/nvim-nio"
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    event = "VeryLazy",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_initialized["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "mfussenegger/nvim-dap",
+    },
+    opts = {
+      handlers = {},
+    },
+  },
+  {
+    "mfussenegger/nvim-dap",
+    config = function(_,_)
+      require("core.utils").load_mappings("dap")
+    end
+  },
+  {
     "neovim/nvim-lspconfig",
     config = function()
       require "plugins.configs.lspconfig"
@@ -10,7 +49,8 @@ local plugins = {
     "williamboman/mason.nvim",
     opts = {
       ensure_installed = {
-        "clangd"
+        "clangd",
+        "codelldb",
       }
     }
   }
